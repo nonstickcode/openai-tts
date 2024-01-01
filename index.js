861803efc13cd4f9ea8269e7e5ac149f43ff590a
Cody <66153585+nonstickcode@users.noreply.github.com>
2023-12-31 18:11:22 -0700

    changed location where audio files save

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fs from "fs"; // filesystem module
 import path from "path"; // path module
 import OpenAI from "openai"; // OpenAI SDK
 
-const speechFile = path.resolve(`./${Date()}.mp3`); // path and filename for the speech file
+const speechFile = path.resolve(`./audio-files/${Date()}.mp3`); // path and filename for the speech file
 import { config } from "dotenv";
 config();
 
@@ -20,8 +20,9 @@ async function main() {
 
       model: "tts-1", // tts-1 or tts-1-hd
       voice: "onyx", // OPTIONS include (alloy, echo, fable, + onyx, nova, and shimmer)
-      input: "Hi Emmett!"
-      // "Hheyy, if I had a voiiice... I'd prolly say, Hey th-… thhank yoou forrr turnin onn thhhe Chadd" // 126 characters = about $0.00189 for tts-1, tts-1-hd is 2x more cost than regular tts-1
+      input: "Hheyy, if I had a voiiice... I'd prolly say, Hey th-… thhank yoou forrr turnin onn thhhe Chadd" 
+      
+      // 126 characters = about $0.00189 for tts-1, tts-1-hd is 2x more cost than regular tts-1
 
     });
 
